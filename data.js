74eac176018ff829a6254f3611443d6b501f2576
Roosterbear <lfpereag@gmail.com>
2019-11-13 14:23:39 -0600

    primer subida

---
+++ data.js
@@ -0,0 +1,7 @@
+let elemento = document.getElementById('titulo')
+//Ver el contenido de la etiqueta
+console.log(elemento.innerHTML)
+//dataset se refiere a elementos data en las etiquetas
+console.error(elemento.dataset.mensaje)
+//en la etiqueta separamos con guiones, aqui con mayusculas
+console.warn(elemento.dataset.mensajeOculto)
---
+++ mensajes.js
@@ -0,0 +1,7 @@
+console.log('este es un mensaje de bitacora')
+console.error('este es un mensaje de error')
+console.warn('este es un mensaje de advertencia')
+console.info('este es un mensaje informativo')
+confirm('Estas seguro que deseas borrar tu disco duro?')
+console.log('Disco Duro borrado en mi imaginacion')
+
