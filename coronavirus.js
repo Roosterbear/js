b0cc44cca0f7c729902b76501d2499061fadf2b9
Luis Perea <lfpereag@gmail.com>
2020-05-24 02:44:06 -0500

    Nuevos ejercicios mayo 2020

---
+++ coronavirus.js
@@ -0,0 +1,28 @@
+const fernando = document.querySelector('.fernando');
+const barranca = document.querySelector('.barranca');
+const aguascalientes = document.querySelector('.aguascalientes');
+const mexico = document.querySelector('.mexico');
+const mundo = document.querySelector('.mundo');
+
+fernando.addEventListener('click',e=>{      
+    e.stopImmediatePropagation();  
+    alert('Te has contaminado');
+});
+
+barranca.addEventListener('click',e=>{
+    alert('Contaminaste la barranca');
+});
+
+aguascalientes.addEventListener('click',e=>{
+    alert('Contaminaste aguascalientes');
+});
+
+mexico.addEventListener('click',e=>{
+    alert('Contaminaste mexico');
+});
+
+mundo.addEventListener('click',e=>{
+    alert('Contaminaste el mundo');
+});
+
+
---
+++ destructing.js
@@ -0,0 +1,9 @@
+const objeto = {
+    nombre: 'Luis',
+    pais: 'Mexico'
+}
+
+let {nombre,pais} = objeto;
+
+console.log(nombre);
+console.log(pais);
