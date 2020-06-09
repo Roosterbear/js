1f4ece7883c8df79393daf5f0b87788c11ceb57d
Luis Perea <lfpereag@gmail.com>
2020-06-08 23:11:46 -0500

    modulos

---
+++ exportar.js
@@ -0,0 +1,6 @@
+export const nombre = 'Fernando Roosterbear';
+export let numero = 5;
+
+export function saludar(){
+    return `Hola ${nombre}, tu numero es ${numero}`;
+}
---
+++ importar.js
@@ -0,0 +1,8 @@
+// Agregar al llamado del script: type="module"
+
+//import {nombre} from './exportar.js';
+//console.log(nombre);
+
+
+//import * as prueba from './exportar.js';
+//console.log(prueba.saludar());
