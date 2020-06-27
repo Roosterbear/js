b6def6c5b18bc17f57b6f47e4843590bc88f4b5f
Luis Perea <lfpereag@gmail.com>
2020-06-27 15:10:28 -0500

    haciendo un ejercicio de localstorage

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function suma(num){
-    return num + 2;
-}
+let nombres = ['Jacinto','Jeremias','Panchito'];
+localStorage.setItem('nombre','nombres');
 
-function resta(num){
-    return num-2;
-}
-
-function multiplicacion(num){
-    return num*2;
-}
-
-function division(num){
-    return num/2;
-}
-
-function operaciones(numero,funcion){
-    return funcion(numero);
-}
-console.log(operaciones(6,suma));
-console.log(operaciones(6,resta));
-console.log(operaciones(6,multiplicacion));
-console.log(operaciones(6,division));
