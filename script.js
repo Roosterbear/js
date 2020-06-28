4f8a9df37ee18cdf7ad84b1cb84c1c4e4b119668
Luis Perea <lfpereag@gmail.com>
2020-06-28 00:46:27 -0500

    actualizacion foreach

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let nombres = ['Jacinto','Jeremias','Panchito'];
-localStorage.setItem('nombre','nombres');
+//destructuring objetos
+/*
+const objeto = {
+    nombre: 'Luis',
+    pais: 'Mexico'
+}
 
+let {nombre,pais} = objeto;
+
+console.log(nombre);
+console.log(pais);
+*/
+
+//destructuring arrays
+/*
+const arreglo = [1,2,3,4,5];
+[uno,dos,tres,cuatro,cinco] = arreglo;
+console.log(uno);
+*/
