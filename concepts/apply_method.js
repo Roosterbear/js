66659f9654e52edcdfb05aa4b4840cbc3060afcc
Roosterbear <lfpereag@gmail.com>
2022-05-22 00:49:44 -0500

    Adding aply method

--- concepts/apply_method.js
+++ concepts/apply_method.js
@@ -1,16 +1,21 @@
-const Producto = {
+const Toalla = {
     descripcion: `Una toalla sucia`,
     precio: 200
+}
+
+const Maleta = {
+    descripcion: `Una maleta muy jodida`,
+    precio: 500
 }
 
 const Compra = {
     descripcion: '',
     precio: 0,
-    darPrecio: function(gracias,por_su_compra){
-        console.log(`Tienes que pagar ${Math.ceil(this.precio*1.15)} por ${this.descripcion} - ${gracias}${por_su_compra}`)
+    darPrecio: function(gracias,por_su_compra, vuelva_pronto){
+        console.log(`Pago de ${Math.ceil(this.precio*1.15)} por ${this.descripcion} - ${gracias} ${por_su_compra}, ${vuelva_pronto}`)
     }
 }
 
-const arreglo = ['Gracias', ' por su compra']
+const arreglo = ['Gracias', ' por su compra', 'vuelva pronto!']
 
-Compra.darPrecio.apply(Producto, arreglo)
+Compra.darPrecio.apply(Maleta, arreglo)
