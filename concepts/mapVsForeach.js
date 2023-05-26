08603d566b5806a995085294cddf069c938974b1
Roosterbear <lfpereag@gmail.com>
2023-05-26 00:00:52 -0600

    Scope Excercice

--- concepts/mapVsForeach.js
+++ concepts/mapVsForeach.js
@@ -12,15 +12,19 @@ const fruits = [
 
 // If we need to keep the values, have to use 'map'
 const fruta1 = fruits.map(function(fruit){
-    return `Si quieres ${fruit.name} te cuesta: $${fruit.price}.00 USD`
-})
+    return `Si quieres ${fruit.name} te cuesta: $${fruit.price}.00 USD [map]`
+});
 
 // We use 'forEach' just for temporary values
 // 'forEach' doesn't keep values
+// We can NOT assign an array to a new variable with forEach
 const fruta2 = fruits.forEach(function(fruit){
-    return `Si quieres ${fruit.name} te cuesta: $${fruit.price}.00 USD`
-})
+    return `Si quieres ${fruit.name} te cuesta: $${fruit.price}.00 USD [forEach]`
+});
 
+console.log(fruta1);
+console.log(fruta2); // Throws undefined
 
-console.log(fruta1)
-console.log(fruta2)
+fruits.forEach(function(fruit){
+    console.log(`Si quieres ${fruit.name} te cuesta: $${fruit.price}.00 USD [forEach alone]`);
+});
