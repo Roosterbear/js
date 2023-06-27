546b4d0c4f43671f746effbff4da74fb1e6a28a7
Roosterbear <fernandoroosterbear@gmail.com>
2023-06-26 19:31:12 -0600

    sets

---
+++ set.js
@@ -0,0 +1,27 @@
+const cart = new Set();
+
+cart.add('Guitar');
+cart.add('Bass Guitar');
+cart.add('Socks');
+
+cart.delete('Socks');
+
+// Delete Set -> cart.clear()
+
+console.log(cart.size);
+
+// Iteretion
+
+cart.forEach(product=>{
+  console.log(product);
+});
+
+
+
+// Deleting duplicate values !!
+
+const values = [1,2,3,4,3,2,1,2,3,4,5,6,7,8,9];
+
+const noDuplicates = new Set(values);
+
+console.log(noDuplicates);
