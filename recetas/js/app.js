fce104af886cf282c934e2f3195e252cd0bb6ce1
Roosterbear <lfpereag@gmail.com>
2023-07-03 21:22:36 -0600

    recetas

---
+++ recetas/js/app.js
@@ -0,0 +1,35 @@
+function iniciarApp(){
+
+  const selectCategorias = document.querySelector('#categorias');
+  selectCategorias.addEventListener('change', seleccionarCategoria)
+
+  obtenerCategorias();
+
+  function obtenerCategorias(){
+    const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
+    fetch(url)
+      .then(respuesta=>respuesta.json())      
+      .then(resultado=>mostrarCategorias(resultado.categories))
+  }
+
+  function mostrarCategorias(categorias = []){
+
+    categorias.forEach(categoria=>{
+      const {strCategory} = categoria;
+      const option = document.createElement('OPTION');
+      option.value = strCategory;
+      option.textContent = strCategory;
+
+      selectCategorias.appendChild(option);
+    })
+  }
+
+  function seleccionarCategoria(e){
+    const categoria = e.target.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+
+    console.log(url);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', iniciarApp);
