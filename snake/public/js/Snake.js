2a176eb27c4bc6766cb77aac1c0c401280d3b80c
Roosterbear <lfpereag@gmail.com>
2023-07-08 13:14:50 -0600

    snake appears!

---
+++ snake/public/js/Snake.js
@@ -0,0 +1,27 @@
+export default class Snake{
+  constructor(scene){
+    this.scene = scene;
+    this.body = [];
+    
+    this.body.push(
+      this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0)
+    );
+    
+    this.body.push(
+      this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0)
+    );
+
+    scene.input.keyboard.on('keydown', e=>{
+      this.keydown(e);
+    });
+  }
+
+  keydown(event){
+    console.log(event);
+  }
+
+  update(time){
+    this.body[0].x += 1;
+    this.body[0].y += 1;
+  }
+}
--- snake/public/js/singlephase.js
+++ snake/public/js/singlephase.js
@@ -1,9 +1,11 @@
+import MainScene from './MainScene.js';
+
 const config = {
   width: 600,
   height: 600,
   type: Phaser.AUTO,
   parent: 'phaser-game',
-  scene: []
+  scene: [MainScene]
 };
 
 new Phaser.Game(config);
