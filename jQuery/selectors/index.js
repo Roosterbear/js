37b04234a14b2e981265b7751eebf506521e3dc2
Roosterbear <lfpereag@gmail.com>
2023-07-08 00:40:18 -0600

    jQuery

---
+++ jQuery/selectors/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".red").css("background", "red")
+                      .css("color", "white")
+                      .css("padding", "5px");
+
+$(".green").css("background", "green")
+.css("color", "white")
+.css("padding", "5px");                      
+
+  
+  
+});
