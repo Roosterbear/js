74d4d439f4fa65df3b47c6f6d5bc729d83d66654
Roosterbear <lfpereag@gmail.com>
2023-07-07 19:07:49 -0600

    working at home

--- jest-test/src/calculator/calculator.test.js
+++ jest-test/src/calculator/calculator.test.js
@@ -1,5 +1,8 @@
-test('Empty test, it will pass', ()=>{
-  //
+const calculator = require('.');
+
+test('Adding 1+2 to test add function...', ()=>{
+  const result = calculator.sum(1,2);
+  expect(result).toBe(3);
 });
 
 test.todo('I will do this, some day...');
