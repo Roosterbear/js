ab669e5d1a3cba6a1aa54f85472b2dbb591b8141
Roosterbear <lfpereag@gmail.com>
2023-07-11 22:28:03 -0600

    exercise 2

--- exams/paths/index.js
+++ exams/paths/index.js
@@ -1,3 +1,46 @@
-function pathSimplify(path){
+/*
 
+We get a Linux absolute path and we have to simplify it.
+
+Rules:
+When there are spaces (double bar) or ".", ommit.
+When there are "..", delete the last folder.
+
+pathSimplify("/home/");             // Output: /home
+pathSimplify("/x/./y/../../z/");    // Output: /z
+pathSimplify("/../");               // Output: /
+pathSimplify("/home//pruebas/");    // Output: /home/pruebas
+ 
+When there are spaces (double bar) or ".", ommit.
+When there are "..", delete the last folder.
+
+*/
+
+function pathSimplify(path){
+  let pile = [];
+
+  const parts = path.split('/');
+
+  for(const part of parts){
+    if(part==='..'){
+      pile.pop();
+    }
+    else if(part !== '.' && part !== ''){
+      pile.push(part);
+    }
+  }
+
+  return '/'+pile.join('/');
 }
+
+console.log(pathSimplify("/home/"));             // Output: /home
+console.log(pathSimplify("/x/./y/../../z/"));    // Output: /z
+console.log(pathSimplify("/../"));               // Output: /
+console.log(pathSimplify("/home//pruebas/"));    // Output: /home/pruebas
+
+/*
+
+Now, do the same with filter and reduce
+
+*/
+ 
