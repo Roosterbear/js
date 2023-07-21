8c3c7327d1b98cca1f9fc0b3b371f62b857637a9
Roosterbear <lfpereag@gmail.com>
2023-07-20 23:12:03 -0600

    exams - fooling with hangman

--- exams/hangman/index.js
+++ exams/hangman/index.js
@@ -2,28 +2,44 @@
 
 Hangman Game
 
-Given a word, we have to guess, letter by letter the hidden word.
+Given a word or phrase, we have to guess, letter by letter the hidden movie.
 
 */
 
 function hangmanGame(movie){
+
   // Keep given word
   const _movie = movie.toUpperCase();
   
   // Array with valid letters
   const validLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                        'L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+                        'L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
   
   // Keep count of wasted letter
   const wastedLetter = [];
 
-  let wordsMovie = _movie.trim().replace('H', 'Mr. H');
+  let wordsMovie = _movie.trim();
 
+  const readline = require('readline');
 
-  //TODO: Welcome and ask for a letter to guess
-  console.log('***** Welcome to the Hangman Game! *****');
-  console.log('Try to guess the hidden movie...');
-  console.log('***** Welcome to the Hangman Game! *****');
+  const rl = readline.createInterface(process.stdin, process.stdout);
+
+  // Welcome and ask for a letter to guess
+  console.log('@@@@@ Welcome to the Hangman Game! @@@@@');
+  console.log('...Try to guess the hidden movie...');
+  console.log('***** You have 5 attempts left *****');
+  console.log('');
+
+  
+  rl.question('Give me a letter: ', (input) => {
+    // El valor ingresado por el usuario estará almacenado en la variable "input"
+    console.log('Valor ingresado:', input);
+    
+
+    // Cierra la interfaz después de obtener la entrada del usuario
+    rl.close();
+  });
+
   //TODO:
   
   //TODO:
